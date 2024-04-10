5c9f545f3603e4e58c767e5eb55cfc90f11bbb57
samarthdaad <161499103+samarthdaad@users.noreply.github.com>
2024-04-10 12:06:14 +0530

    Create k8s_repo.sh

---
+++ k8s_repo.sh
@@ -0,0 +1,5 @@
+osm repo-add --type helm-chart open5gsnrf http://hasanyeganeh.github.io/open5gs-nrf/
+osm repo-add --type helm-chart open5gscp http://hasanyeganeh.github.io/open5gs-cp/
+osm repo-add --type helm-chart towards5gc https://raw.githubusercontent.com/Orange-OpenSource/towards5gs-helm/main/repo/
+osm repo-add --type helm-chart open5gs https://adaptivenetlab.github.io/charts/
+osm repo-add --type helm-chart ueransim https://adaptivenetlab.github.io/charts/
